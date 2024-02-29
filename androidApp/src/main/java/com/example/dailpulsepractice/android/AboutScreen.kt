d9fc333e8c3097cf2a0935afd5a185b2c29fcafd
Thulani Mtetwa <thulanimtetwa@gmail.com>
2024-02-29 15:26:43 +0200

    Add About screen for android app

---
+++ androidApp/src/main/java/com/example/dailpulsepractice/android/AboutScreen.kt
@@ -0,0 +1,67 @@
+package com.example.dailpulsepractice.android
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.dailpulsepractice.Platform
+
+@Composable
+fun AboutScreen() {
+    Column {
+        Toolbar()
+        ContentView()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Toolbar() {
+    TopAppBar(
+        title = { Text(text = "About Device") }
+    )
+}
+
+@Composable
+private fun ContentView() {
+    var items = makeItems()
+
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(items) { row ->
+            RowView(title = row.first, subtitle = row.second)
+        }
+    }
+}
+
+private fun makeItems(): List<Pair<String, String>> {
+
+    val platform = Platform()
+    platform.logSystemInfo()
+
+    return listOf(
+        Pair("Operating System", "${platform.name} ${platform.osVersion}"),
+        Pair("Device", "${platform.deviceModel}"),
+        Pair("Density", "${platform.density.toString()}")
+    )
+}
+
+@Composable
+private fun RowView(title: String, subtitle: String) {
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Column(Modifier.padding(8.dp)) {
+            Text(text = title, style = MaterialTheme.typography.bodySmall, color = Color.Gray)
+            Text(text = subtitle, style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
